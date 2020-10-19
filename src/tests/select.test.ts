c01d5c90c1b0ef5de90872ea68dca8f1f3dd0510
linrium <linrium@gmail.com>
2020-10-19 14:59:59 +0700

    update

--- src/tests/select.test.ts
+++ src/tests/select.test.ts
@@ -124,3 +124,75 @@ describe('select', () => {
     })
   })
 })
+
+
+let testCreateConfig = `\
+-- hello
+create config test
+-- hello
+`
+
+let testGetConfig = `\
+-- hello
+get config (
+  -- hello
+  id = 43234,
+  type = 'metabase'
+)
+-- hello
+`
+
+let testUpdateConfig = `\
+-- hello
+update config
+-- hello
+`
+
+let testCreateWidget = `\
+-- hello
+create widget test (
+  id = 1234,
+  type = 'vertical-bar',
+  column = 'name'
+)
+-- hello
+`
+
+let testFetch = `\
+-- hello
+fetch title38 (
+  query = 'nearby order_stop',
+  filter = {
+    "order_status": ["=", 1]
+  }
+)
+-- hello
+`
+
+
+let testSelect = `\
+select * from m
+join t on t.id = m.order_id
+where t.id='order-stop'
+and m.id = 21041
+and m.limit = 1
+and t.limit = 2
+and t.status = ['online', 'busy']
+and t.order_status in ('ASSIGNING', 'IN_PROCESS')
+and t.order_stop=1
+and m.service = 'SGN-BIKE'
+and t.service in ['SGN-BIKE','SGN-POOL']
+and t.nearby in point(current_points,1000)
+and m.cast_to = 't'
+and m.id_field = 'order_id'
+and m.geometry = {
+  "type": "Point",
+  "fields": ["lng", "lat"]
+}
+and t.filter = {
+  "order_id": [
+     "20MAMP72"
+  ]
+}
+limit 10
+`
